9beed9dacae668572417aa484aa3b254af3a21e1
liweijia12 <liweijia@buffalo.edu>
2019-04-04 02:12:45 -0400

    reorganized code into specific files for ease of use

--- game.py
+++ game.py
@@ -1,68 +1,7 @@
 import pygame
 import random
-
-# color palette
-black = (0, 0, 0)
-yellow = (255, 255, 0)
-blue = (100, 255, 255)
-green = (0, 255, 0)
-red = (255, 0, 0)
-white = (255, 255, 255)
-gray = (105, 105, 105)
-navy = (0, 0, 128)
-bluegray = (112, 138, 144)
-
-# PRIMARY FUNCTIONS
-def avatar(px, py, username, GUI):
-    pygame.draw.rect(GUI, yellow, [px, py, 20, 10])
-    font = pygame.font.SysFont(None, 30)
-    text = font.render(username, True, red)
-    GUI.blit(text, [px - 40, py - 50])
-
-
-def gameover(GUI):
-    GUI.fill(white)
-    font = pygame.font.SysFont(None, 60)
-    text = font.render("Game Over!", True, red)
-    text2 = font.render("Press Esc to Quit.", True, red)
-    GUI.blit(text, [150, 250])
-    GUI.blit(text2, [150, 300])
-
-
-def toppipe(xpos, ypos, width, length, GUI):
-    pygame.draw.rect(GUI, bluegray, [xpos, ypos, width, length])
-
-
-def bottompipe(xpos, ypos, width, length, space, GUI):
-    yposb = ypos+length+space
-    lengthb = length + 1000
-    pygame.draw.rect(GUI, bluegray, [xpos, yposb, width, lengthb])
-
-
-def ceiling(GUI):
-    pygame.draw.rect(GUI, bluegray, [0, 0, 700, 50])
-
-
-def ground(GUI):
-    pygame.draw.rect(GUI, bluegray, [0, 500, 700, 50])
-
-
-def Instruction(GUI):
-    font = pygame.font.SysFont(None, 25)
-    text = font.render("Press Any Key to Begin, Up Down Left Right Arrow to Move, Avoid obstacles", True, black)
-    GUI.blit(text, [0, 500])
-
-
-def groundCollision(py):
-    if py >= 480:
-        py = 480
-    return py
-
-
-def ceilingCollision(py):
-    if py <= 50:
-        py = 50
-    return py
+from gamefunc import *
+from colorpalette import *
 
 
 def startGame():
--- unittesting.py
+++ unittesting.py
@@ -1,33 +1,8 @@
 import unittest
-from game import groundCollision
-from game import ceilingCollision
+from gamefunc import groundCollision
+from gamefunc import ceilingCollision
 import pygame
 import sys
-
-
-# def testCONTROLSREGISTER():
-#
-#     pygame.init
-#     pygame.display.set_mode((100, 100))
-#     pygame.display.set_caption('Keyboard Register')
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.KEYDOWN:
-#                 if event.key == pygame.K_LEFT:
-#                     print('LEFT ARROW PRESSED')
-#                 if event.key == pygame.K_RIGHT:
-#                     print('RIGHT ARROW PRESSED')
-#                 if event.key == pygame.K_UP:
-#                     print('UP ARROW PRESSED')
-#                 if event.key == pygame.K_DOWN:
-#                     print('DOWN ARROW PRESSED')
-#                 if event.key == pygame.K_ESCAPE:
-#                     print('ESCAPE KEY PRESSED')
-#                     sys.exit()
-#
-#         pygame.display.flip()
-#
-# testCONTROLSREGISTER()
 
 
 class TestLimits(unittest.TestCase):
