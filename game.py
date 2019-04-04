bf4a1c0de45118ea8d861b9c85c639c10fec1124
liweijia12 <liweijia@buffalo.edu>
2019-04-04 01:51:52 -0400

    cleaned up codes

--- game.py
+++ game.py
@@ -12,41 +12,7 @@ gray = (105, 105, 105)
 navy = (0, 0, 128)
 bluegray = (112, 138, 144)
 
-# # screen details
-# WIDTH = 700
-# HEIGHT = 550
-# FPS = 60
-#
-# screensize = WIDTH, HEIGHT
-#
-# GUI = pygame.display.set_mode(screensize)
-#
-# pygame.display.set_caption("AVOID THE OBSTACLES")
-
-
-# IN-GAME VALUES
-
-# # avatar position and movement
-# px = 150
-# py = 150
-#
-# # object positions
-# xpos= 700
-# ypos = 0
-#
-# # object size- randomized
-# width = random.randint(30, 80)
-# length = random.randint(30, 350)
-# space = random.randint(20, 100)
-#
-# # speed
-# xspeed = 0
-# yspeed = 0
-# movespeed = 6
-# stopspeed = 0
-# pipspeed = 0
-
-
+# PRIMARY FUNCTIONS
 def avatar(px, py, username, GUI):
     pygame.draw.rect(GUI, yellow, [px, py, 20, 10])
     font = pygame.font.SysFont(None, 30)
@@ -95,116 +61,6 @@ def ceilingCollision(py):
     return py
 
 
-
-# pygame.init()
-#
-# gameFINISH = False
-# gameOVER = False
-#
-# clock = pygame.time.Clock()
-#
-# while not gameFINISH:
-#     # background
-#
-#     GUI.fill(navy)
-#
-#     # top pipe
-#     toppipe(xpos, ypos, width, length)
-#
-#     # bottom pipe
-#     bottompipe(xpos, ypos, width, length)
-#
-#     # player
-#     avatar(px, py, name)
-#
-#     # ceiling
-#     ceiling()
-#
-#     # ground
-#     ground()
-#
-#     # instructions
-#     Instruction()
-#
-#     py += yspeed
-#     px += xspeed
-#     xpos -= pipspeed
-#
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             gameFINISH = True
-#
-#         # CONTROLS
-#         if event.type == pygame.KEYDOWN:
-#
-#             # START MOVING PIPES
-#             pipspeed = 4
-#
-#             if event.key == pygame.K_UP:
-#                 yspeed = -movespeed
-#             if event.key == pygame.K_DOWN:
-#                 yspeed = movespeed
-#             if event.key == pygame.K_RIGHT:
-#                 xspeed = movespeed
-#             if event.key == pygame.K_LEFT:
-#                 xspeed = -movespeed
-#
-#         if event.type == pygame.KEYUP:
-#             if event.key == pygame.K_UP:
-#                 yspeed = stopspeed
-#             if event.key == pygame.K_DOWN:
-#                 yspeed = stopspeed
-#             if event.key == pygame.K_RIGHT:
-#                 xspeed = stopspeed
-#             if event.key == pygame.K_LEFT:
-#                 xspeed = stopspeed
-#
-#         # REAL TIME UNIT TESTING
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_LEFT:
-#                 print('LEFT ARROW PRESSED')
-#             if event.key == pygame.K_RIGHT:
-#                 print('RIGHT ARROW PRESSED')
-#             if event.key == pygame.K_UP:
-#                 print('UP ARROW PRESSED')
-#             if event.key == pygame.K_DOWN:
-#                 print('DOWN ARROW PRESSED')
-#             if event.key == pygame.K_ESCAPE:
-#                 print('ESCAPE KEY PRESSED')
-#                 quit()
-#     # COLLISIONS
-#
-#     # top pipe collision
-#     if xpos - 20 < px < xpos + width and py < ypos+length:
-#         gameOVER = True
-#     # bottom pipe collision
-#     if xpos - 20 < px < xpos + width and py > ypos+length+space-10:
-#         gameOVER = True
-#
-#     # ceiling collision
-#     if py <= 50:
-#         py = 50
-#
-#     # ground collision
-#     if py >= 480:
-#         py = 480
-#
-#
-#     # reset and re-randomize pipes
-#     if xpos < -80:
-#         xpos = 700
-#         length = random.randint(30, 450)
-#         space = random.randint(20, 100)
-#
-#     if gameOVER:
-#         gameover()
-#
-#
-#     pygame.display.flip()
-#     clock.tick(FPS)
-#
-# pygame.quit()
-
 def startGame():
 
     pygame.init()
@@ -213,9 +69,7 @@ def startGame():
     WIDTH = 700
     HEIGHT = 550
     FPS = 60
-
     screensize = WIDTH, HEIGHT
-
     GUI = pygame.display.set_mode(screensize)
 
     pygame.display.set_caption("AVOID THE OBSTACLES")
@@ -261,14 +115,14 @@ def startGame():
         # bottom pipe
         bottompipe(xpos, ypos, width, length, space, GUI)
 
-        # player
-        avatar(px, py, name, GUI)
-
         # ceiling
         ceiling(GUI)
 
         # ground
         ground(GUI)
+
+        # player
+        avatar(px, py, name, GUI)
 
         # instructions
         Instruction(GUI)
@@ -350,5 +204,6 @@ def startGame():
         clock.tick(FPS)
 
     pygame.quit()
+
 
 startGame()
